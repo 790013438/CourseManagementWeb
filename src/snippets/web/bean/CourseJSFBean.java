959dfc4fbf5c394d5f38c8d9b4aba3ddebe9c04d
790013438 <790013438@qq.com>
2017-09-08 10:13:14 +0800

    jsf managed bean

---
+++ src/snippets/web/bean/CourseJSFBean.java
@@ -0,0 +1,19 @@
+package snippets.web.bean;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+
+import snippets.dto.CourseDTO;
+import snippets.ejb.CourseBeanRemote;
+
+@ManagedBean(name="Course")
+public class CourseJSFBean {
+    @EJB
+    CourseBeanRemote courseBean;
+
+    public List<CourseDTO> getCourses() {
+        return courseBean.getCourses();
+    }
+}
